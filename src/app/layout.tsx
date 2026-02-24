201aaba490eed7ac153ae560ba01857ef8ab91f1
andrew <andrewpierno@gmail.com>
2026-02-24 06:39:37 -0800

    launch posts: Show HN, LinkedIn, Twitter thread

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistMono = Geist_Mono({
